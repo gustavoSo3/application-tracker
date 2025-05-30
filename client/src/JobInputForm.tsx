48108f6d3b33edf0a1d7773130f2aa9f3c7caa05
Gustavo Sotres <gus.sotres@gmail.com>
2025-05-30 14:25:45 -0700

    Created basic input html code
    Included all inputs to fill a JobCardProps object

--- client/src/JobInputForm.tsx
+++ client/src/JobInputForm.tsx
@@ -1,9 +1,68 @@
+import { JobApplicationStatus } from "./Types"
+
 export function JobInputForm() {
 
-
 	return (
-		<div>
+		<div className="w-2xl p-5 border-gray-500 rounded-2xl border-2 flex flex-col">
 
+			<form className="mb-5 flex flex-col [&>*]:pt-1.5 [&>*]:pb-1.5 [&>*]:pl-3 [&>*]:mt-1">
+				
+				<div className="border-1 border-gray-400 rounded-sm flex">
+					<label htmlFor="job-title" className="p-0.5 flex-1 text-lg">Job Title</label>
+					<input id="job-title" name="job-title" className="flex-2" type="text" placeholder="Job title"/>
+				</div>
+				
+				<div className="border-1 border-gray-400 rounded-sm flex">
+					<label htmlFor="company-name" className="p-0.5 flex-1 text-lg">Company Name</label>
+					<input id="company-name" name="company-name" className="flex-2" type="text" placeholder="Company Name"/>
+				</div>
+
+				<div className="border-1 border-gray-400 rounded-sm flex">
+					<label htmlFor="location" className="p-0.5 flex-1 text-lg">Location</label>
+					<input id="location" name="location" className="flex-2" type="text" placeholder="Location"/>
+				</div>
+
+				<div className="border-1 border-gray-400 rounded-sm flex">
+					<label htmlFor="date" className="p-0.5 flex-1 text-lg">Date</label>
+					<input id="date" name="date" className="flex-2" type="date"/>
+				</div>
+			
+				<div className="border-1 border-gray-400 rounded-sm flex">
+					<label htmlFor="status" className="p-0.5 flex-1 text-lg">Status</label>
+					<select id="status" name="status" className="flex-2" defaultValue={JobApplicationStatus.None}>
+						{
+							Object.values(JobApplicationStatus).map((status) => (
+								<option key={status} value={status}>{status}</option>
+							))
+						}
+					</select>
+				</div>
+
+				<div className="border-1 border-gray-400 rounded-sm flex">
+					<label htmlFor="url" className="p-0.5 flex-1 text-lg">URL</label>
+					<input id="url" name="url" className="flex-2" type="url" placeholder="URL"/>
+				</div>
+
+				<div className="border-1 border-gray-400 rounded-sm flex">
+					<label htmlFor="description" className="p-0.5 flex-1 text-lg">Notes</label>
+					<textarea id="description" name="description" className="flex-2 border-1 border-dotted border-gray-200" cols={50} rows={8} placeholder="Something you need to remember?"></textarea>
+				</div>
+			
+			</form>
+
+			<button type="button" className="text-xl p-2 bg-blue-400 border-blue-700 hover:bg-blue-600 hover:text-white border-1 rounded-4xl">Add to list</button>
 		</div>
 	)
 }
+
+/*
+JobCardProps = {
+	jobTitle: string,
+	companyName: string,
+	location: string,
+	appliedDate: Date,
+	status: JobApplicationStatus,
+	url: URL,
+	description: string
+}
+*/
